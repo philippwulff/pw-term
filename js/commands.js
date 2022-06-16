5b0473527c2405b55ec96f57617666fd2eeb8b14
philippwulff <philipp.sylt@gmail.com>
2022-06-16 23:16:11 +0200

    working sceleton

---
+++ js/commands.js
@@ -0,0 +1,84 @@
+// var youtube = "https://www.youtube.com/fknight/";
+// var twitter = "https://www.twitter.com/forrestpknight/";
+//var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/philippwulff/";
+var github = "https://github.com/philippwulff";
+var email = 'mailto:philipp.wulff@tum.de';
+
+about = [
+  "<br>",
+  "I am Philipp, a computer science student at the Technical University of Munich.",
+  "<br>"
+];
+
+// whoami = [
+//   "<br>",
+//   "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+//   "<br>"
+// ];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/philippwulff' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/philippwulff' + "</a>",
+  "<br>"
+];
+
+// secret = [
+//   "<br>",
+//   '<span class="command">sudo</span>           Only use if you\'re admin',
+//   "<br>"
+// ];
+
+projects = [
+  "<br>",
+  "Work in progress... most projects are on GitHub or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>          Who is Philipp?',
+  '<span class="command">social</span>         Display social networks',
+  // '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+// banner = [
+//   '<span class="index">Philipp Wulff. All rights reserved.</span>',
+//   "         ___   ____",
+//   "       /' --;^/ ,-_\\     \\ | /       ",
+//   "      / / --o\\ o-\\ \\\\   --(_)--   ",
+//   "     /-/-/|o|-|\\-\\\\|\\\\   / | \\   ______                          __     __ __       _       __    __   ",
+//   "      '`  ` |-|   `` '          / ____/___  _____________  _____/ /_   / //_/____  (_)___ _/ /_  / /_ ",
+//   "            |-|                / /_  / __ \\/ ___/ ___/ _ \\/ ___/ __/  / ,<  / __ \\/ / __ `/ __ \\/ __/ ",
+//   "            |-|O              / __/ / /_/ / /  / /  /  __(__  ) /_   / /| |/ / / / / /_/ / / / / /_  ",
+//   "            |-(\\,__          /_/    \\____/_/  /_/   \\___/____/\\__/  /_/ |_/_/ /_/_/\\__, /_/ /_/\\__/ ",
+//   "         ...|-|\\--,\\_....                                                         /____/    © 2022",
+//   "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
+//   "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+//   '<span class="color2">Welcome to my interactive web terminal.</span>',
+//   "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+// ];
+
+banner = [
+  // '<span class="index">Philipp Wulff. All rights reserved.</span>',
+  "██████╗ ██╗  ██╗██╗██╗     ██╗██████╗ ██████╗     ██╗    ██╗██╗   ██╗██╗     ███████╗███████╗",
+  "██╔══██╗██║  ██║██║██║     ██║██╔══██╗██╔══██╗    ██║    ██║██║   ██║██║     ██╔════╝██╔════╝",
+  "██████╔╝███████║██║██║     ██║██████╔╝██████╔╝    ██║ █╗ ██║██║   ██║██║     █████╗  █████╗  ",
+  "██╔═══╝ ██╔══██║██║██║     ██║██╔═══╝ ██╔═══╝     ██║███╗██║██║   ██║██║     ██╔══╝  ██╔══╝  ",
+  "██║     ██║  ██║██║███████╗██║██║     ██║         ╚███╔███╔╝╚██████╔╝███████╗██║     ██║     ",
+  "╚═╝     ╚═╝  ╚═╝╚═╝╚══════╝╚═╝╚═╝     ╚═╝          ╚══╝╚══╝  ╚═════╝ ╚══════╝╚═╝     ╚═╝     v1.0",                                                                                      
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+gui = [
+
+];
